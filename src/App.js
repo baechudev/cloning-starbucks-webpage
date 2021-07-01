afb4f1c5d825cda708db1acd9d740a6a7004525b
Dongwook Hwang <dwhwang423@gmail.com>
2021-07-01 19:56:24 -0400

    main content proto done

--- src/App.js
+++ src/App.js
@@ -21,6 +21,18 @@ function App() {
             direction={data.direction}
           />
         ))}
+        <div className="additionalExp">
+          <p>
+            *At participating stores. Some restrictions apply. See
+            starbucks.ca/rewards.
+          </p>
+          <p>
+            **Menu limited. Restricted delivery area. Available in participating
+            locations only. Fees subject to change. Prices for Starbucks® items
+            purchased through Uber Eats may be higher than posted in stores or
+            as marked. See the Uber Eats app for details.
+          </p>
+        </div>
       </div>
     </div>
   );
